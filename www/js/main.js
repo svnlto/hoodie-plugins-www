8ade535ae113ae44c3380f06809f65530cb6e60f
Alan Shaw <alan@freestyle-developments.co.uk>
2013-11-12 21:45:32 +0000

    Super simple search (actually filter)

--- www/js/main.js
+++ www/js/main.js
@@ -2,11 +2,32 @@
   var hoodie  = new Hoodie()
     , store = hoodie.open("hoodie-plugin-plugins")
     , pluginsList = $("#plugins-list")
+    , searchField = $("#search")
 
-  var reactive = hoodie.reactive(pluginsList, $("script", pluginsList).text(), function (store) {
+  function filterList () {
+    var q = searchField.val().toLowerCase()
+      , plugins = $("a", pluginsList)
+
+    if (!q) return plugins.removeClass("hidden")
+
+    plugins.each(function () {
+      var plugin = $(this)
+      if (plugin.text().toLowerCase().indexOf(q) == -1) {
+        plugin.addClass("hidden")
+      }
+    })
+  }
+
+  searchField.keyup(filterList)
+
+  hoodie.reactive(pluginsList, $("script", pluginsList).text(), function (store) {
     var defer = hoodie.defer()
 
     store.findAll("plugin").done(function (plugins) {
+      plugins.forEach(function (p) {
+        p.name = p.id.replace("hoodie-plugin-", "")
+      })
+      defer.then(filterList)
       defer.resolve({plugins: plugins})
     })
 
@@ -14,5 +35,6 @@
   }, {store: store})
 
   store.connect()
+
 })()
 
